f1e0beb8c5bae80868f7e6ac2d8471b6215f3fd2
Rostadhi <rostadhi@gmail.com>
2022-01-12 09:54:55 +0700

    Initial Commit

---
+++ MovieDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDB
+//
+//  Created by Rostadhi Akbar, M.Pd on 12/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Rostadhi Akbar, M.Pd on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
